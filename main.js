03d0ad5d2a6268063c21242d9a36b3cd845d46e3
callmesiva <sgnathan10@gmail.com>
2022-08-11 23:18:11 +0530

    using for-loop make changes

--- main.js
+++ main.js
@@ -1,26 +1,8 @@
-// console.dir(document.title)
 
-// document.title = "My-First-Page";
-
-// console.dir(document.title)
-
-// console.dir(document.all);
-
-// //document.all[20].innerHTML = "hiiiii";
-
-// var head = document.getElementById("main-header");
-// head.style.borderBottom = "solid 6px red";
-
-
-
-
-// let header = document.getElementById('main');
-// let h1 = header.childNodes[1];
-// h1.innerHTML = '<b>ADD ITEMS</b>'
-// h1.style.color = "green"
 
 let item = document.getElementsByClassName("list-group-item");
 item[2].style.backgroundColor = "green";
 
-let allElement = document.getElementsByClassName("list-group")
-allElement[0].style.fontWeight = 'bold';
+for (let i = 0; i < item.length; i++) {
+    item[i].style.fontWeight = 'bold';
+}
