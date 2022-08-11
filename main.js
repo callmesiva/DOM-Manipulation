c072ccc9a3ae126015d8b38abc82ca8b946ec36c
callmesiva <sgnathan10@gmail.com>
2022-08-11 23:06:50 +0530

    Select Element by Class

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
-console.dir(document.title)
+// console.dir(document.title)
 
-document.title = "My-First-Page";
+// document.title = "My-First-Page";
 
-console.dir(document.title)
+// console.dir(document.title)
 
-console.dir(document.all);
+// console.dir(document.all);
 
-//document.all[20].innerHTML = "hiiiii";
+// //document.all[20].innerHTML = "hiiiii";
 
-var head = document.getElementById("main-header");
-head.style.borderBottom = "solid 6px red";
-
-
+// var head = document.getElementById("main-header");
+// head.style.borderBottom = "solid 6px red";
 
 
-let header = document.getElementById('main');
-let h1 = header.childNodes[1];
-h1.innerHTML = '<b>ADD ITEMS</b>'
-h1.style.color = "green"
 
+
+// let header = document.getElementById('main');
+// let h1 = header.childNodes[1];
+// h1.innerHTML = '<b>ADD ITEMS</b>'
+// h1.style.color = "green"
+
+let item = document.getElementsByClassName("list-group-item");
+item[2].style.backgroundColor = "green";
+
+let allElement = document.getElementsByClassName("list-group")
+allElement[0].style.fontWeight = 'bold';
