bf373950c8f8c51876ad087489a72d61d427fc60
callmesiva <sgnathan10@gmail.com>
2022-08-12 12:23:22 +0530

    QuerySelector and QuerySelectorAll

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 
 
-//let list = document.getElementsByClassName('list-group-item')
+// //let list = document.getElementsByClassName('list-group-item')
 
-let list = document.getElementsByTagName('li')
+// let list = document.getElementsByTagName('li')
 
 
-for (let i = 0; i < list.length; i++) {
-    list[i].style.backgroundColor = 'red';
+// for (let i = 0; i < list.length; i++) {
+//     list[i].style.backgroundColor = 'red';
+// }
+
+// Query Selector
+let item2 = document.querySelector('.list-group-item:nth-child(2)');
+item2.style.backgroundColor = "green";
+
+let item3 = document.querySelector('.list-group-item:nth-child(3)');
+item3.style.display = 'none';
+
+//Query Selector ALL
+let fcolor = document.querySelectorAll('.list-group-item');
+fcolor[1].style.color = "white";
+
+let odd = document.querySelectorAll('.list-group-item:nth-child(odd)');
+
+for (let i = 0; i < odd.length; i++) {
+    odd[i].style.backgroundColor = "grey";
 }
